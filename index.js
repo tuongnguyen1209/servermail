3efd8dfafb6da4b8993bc1ad7762686a125c8e49
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2023-05-25 13:42:59 +0700

    commmit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3066;
 
 dotenv.config({ path: `${__dirname}/src/config/config.env` });
 
-// database.connect();
+database.connect();
 
 app.listen(PORT, () => {
   console.log(`App run on port ${PORT}`);
