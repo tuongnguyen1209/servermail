42e5e8487883b02a489e1eb7cffa4e5ebf232bed
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2023-05-25 13:39:59 +0700

    delete

--- src/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const routerV1 = require("./router/v1/");
-
-const router = express.Router();
-
-router.use("/api/v1", routerV1);
-
-module.exports = router;
--- src/router/v1/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const mailCtl = require("../../controller/mailController");
-
-const router = express.Router();
-
-router.route("/sendmail").post(mailCtl.sendmail);
-
-module.exports = router;
